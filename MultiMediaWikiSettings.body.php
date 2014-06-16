8d32f3a86ad0b8a70b5e2f1f347b68b7d7efad1e
Olivier Maridat <olivier.maridat@trialog.com>
2014-06-16 17:00:35 +0200

    Update Mode 1 "by domain" configuration

--- MultiMediaWikiSettings.body.php
+++ MultiMediaWikiSettings.body.php
@@ -1,70 +1,80 @@
 <?php
 /*
-== Manage multiple Wikis using ==
-* 1 MediaWiki installation
-* 1 database (each installation can use different tables by configuring a prefix)
-*/
-
+ * Manage multiple Wikis using
+ * * 1 MediaWiki installation
+ * * 1 database (each installation can use different tables by configuring a prefix)
+ */
+ 
 // -- Select the relevant Wiki
-$server=@$_SERVER["SERVER_NAME"];
-if(!empty($server) && $server!="wiki.trialog.com" && $server!="localhost"){
-    switch($server)
-    {
-        case "wiki.paris-project.org":
-            require_once $IP.'/LocalSettings_WikiParis.php';
-            break;
-        case "wiki.pripare.eu":
-            require_once $IP.'/LocalSettings_WikiPripare.php';
-            break;
-		case "gerontech-aphp.trialog.com":
-            require_once $IP.'/LocalSettings_Wikicgaphp.php';
-            break;
-        default:
-            echo 'This domain is not configured.';
-            exit(0);
-    }
+// Mode 1: by domain name
+$wikiCode = '';
+$server = @$_SERVER["SERVER_NAME"];
+if (! empty($server) && $server != "wiki.trialog.com" && $server != "localhost"
+	&& !empty($wgMultiMediaWikiServers) && array_key_exists($server, $wgMultiMediaWikiServers)) {
+	$wgServer = 'http://'.$server;
+	$wikiCode = $wgMultiMediaWikiServers[$server];
+	$wgDBprefix = 'wiki'.$wikiCode.'_'; 
+	$wgUsePathInfo = true;
+	$wgWikiAddress = $wgServer;
+	$wgScriptPath = $wgServer.'/';
+	$wgScript = $wgScriptPath;
+	$wgLoadScript = '/load'.$wgScriptExtension;
+	$wgStylePath = '/skins';
+	$wgArticlePath = '/wiki/$1';
+	$wgUploadPath = '/images/'.$wikiCode;
+	$wgUploadDirectory = $IP.'/images/'.$wikiCode;
+	$wgFileCacheDirectory = $wgUploadDirectory.'/cache';
+	$wgCacheDirectory = $IP.'/cache/'.$wikiCode;
+	$wgLogo = $wgUploadPath.'/logo/wiki.png';
+	require_once $IP . '/LocalSettings_Wiki'.ucfirst($wikiCode).'.php';  
 }
-else{
-    // Default Wiki
-    $wikiCode = 'Trialog';
-    if (isset($maintenance)) {
-    	// Small change to Maintenance.php available
-    	if (in_array('hasOption', get_class_methods($maintenance)) && $maintenance->hasOption('wiki')) {
-    		$_GET['wiki'] = $maintenance->getOption('wiki', 'Trialog');
-    	}
-    	// Normal MediaWiki version
-    	else {
-			ob_start();
-			$maintenance->globals();
-			$strGlobals = ob_get_clean();
-			if (preg_match('!--wiki!', $strGlobals)) {
-				$_GET['wiki'] = preg_replace('!^.*--wiki\n\s*\[\d+\]\s*=>\s*([a-zA-Z -]+)\s*\n.*$!s', '$1', $strGlobals);
-			}
+// Mode 2: by folder name
+else {
+	// Check maintenance mode for installation process
+	if (isset($maintenance)) {
+		// Small change to Maintenance.php available
+		if (in_array('hasOption', get_class_methods($maintenance)) && $maintenance->hasOption('wiki')) {
+		    $_GET['wiki'] = $maintenance->getOption('wiki', 'Trialog');
+		}         // Normal MediaWiki version
+		else {
+		    ob_start();
+		    $maintenance->globals();
+		    $strGlobals = ob_get_clean();
+		    if (preg_match('!--wiki!', $strGlobals)) {
+			$_GET['wiki'] = preg_replace('!^.*--wiki\n\s*\[\d+\]\s*=>\s*([a-zA-Z -]+)\s*\n.*$!s', '$1', $strGlobals);
+		    }
 		}
-		$wgShowExceptionDetails = true; 
+		$wgShowExceptionDetails = true;
 		// if (NULL != $maintenance && NULL != ($options = @$maintenance->getOption("wiki"))) {
-			// die('Maintenance!'.$options);
-			// $_GET['wiki'] = $options;
+		// die('Maintenance!'.$options);
+		// $_GET['wiki'] = $options;
 		// }
 	}
+	    
+	// Selected Wiki
+	if (isset($_GET['wiki']) && null != $_GET['wiki'] && '' != $_GET['wiki']) {
+		$wikiCode = htmlentities(trim($_GET['wiki']));
+		$wgUsePathInfo = true;
+		$wgDBprefix = 'wiki'.lcfirst($wikiCode).'_'; 
+		$wgWikiAddress = $wgServer.'/'.$wikiCode;
+		$wgSitename = 'Wiki '.ucfirst($wikiCode);
+		$wgScriptPath = $wgScriptPath.'/'.$wikiCode;
+		$wgScript = $wgScriptPath;
+		$wgStylePath = $wgScriptPath.'/skins';
+		$wgLoadScript = $wgScriptPath.'/load'.$wgScriptExtension;
+		$wgArticlePath = $wgScript.'/$1';
+		$wgUploadPath = $wgScriptPath.'/images';
+		$wgUploadDirectory = $IP.'/images/'.$wikiCode;
+		$wgFileCacheDirectory = $wgUploadDirectory.'/cache';
+		$wgCacheDirectory = $IP.'/cache/'.$wikiCode;
+		$wgLogo = $wgUploadPath.'/logo/wiki.png';
+	}
+}
 
-    // Selected Wiki
-    if (isset($_GET['wiki']) && null != $_GET['wiki'] && '' != $_GET['wiki']) {
-        $wikiCode = htmlentities(trim($_GET['wiki']));
-	$wgScriptPath = $wgScriptPath.'/'.$wikiCode;
-	$wgScript = $wgScriptPath;
-        $wgWikiAddress = $wgServer.'/'.$wikiCode;
-        $wgUsePathInfo	= true;
-    }
-
-    // -- Launch the selected wiki configuration
-    if(file_exists($IP.'/LocalSettings_Wiki'.$wikiCode.'.php')) {
-        require_once ($IP.'/LocalSettings_Wiki'.$wikiCode.'.php');
-    }
-    // This Wiki doesn't exist: allow the user to create it
-    else {
-    //	require_once( "$IP/includes/templates/NoLocalSettings.php" );
-        // TODO: gérer l'installation du nouveau sub-wiki
-        die('No Settings yet! Create the file: '.$IP.'/LocalSettings_Wiki'.$wikiCode.'.php first!'.(isset($maintenance) ? ' Hint: don\'t forget --globals!' : ''));
-    }
+// Launch the selected wiki configuration
+if (file_exists($IP . '/LocalSettings_Wiki'.ucfirst($wikiCode).'.php')) {
+	require_once($IP . '/LocalSettings_Wiki'.ucfirst($wikiCode).'.php');
+}
+else {
+	die('This wiki doesn\'t exist, sorry!');
 }
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-multimediawiki
-==============
+MultiMediaWiki Extension
+========================
 
 An extension to manage several wikis with one MediaWiki installation
+
+Usage
+----
+Add the following before your other extensions in LocalSettings.php
+
+	$wgMultiMediaWikiServers = array(); // $_SERVER["SERVER_NAME"] => wiki code
+	$wgMultiMediaWikiServers['yoursubwiki.domain.com'] = 'wiki1';
+	$wgMultiMediaWikiServers['yourotherwiki.domain.com'] = 'wiki2';
+	require_once($IP.'/extensions/multimediawiki/MultiMediaWiki.php');
+
+Then add one file per wiki for specific configuration: LocalSettings_WikiWiki1.php and LocalSettings_WikiWiki2.php.
+
+License
+----
+This MediaWiki extension has been made by Trialog under the LGPL3+ license. It is a work in progress and every feedbacks are welcome!
